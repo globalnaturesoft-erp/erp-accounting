16d689b5562c2da30a674a296f3191db03022f07
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-02-16 18:39:43 -0800

    change get order all to order search(params)

--- app/controllers/erp/accounting/backend/orders_controller.rb
+++ app/controllers/erp/accounting/backend/orders_controller.rb
@@ -7,7 +7,7 @@ module Erp
     
         # POST /orders/list
         def list
-          @orders = Erp::Orders::Order.all.paginate(:page => params[:page], :per_page => 5)
+          @orders = Erp::Orders::Order.search(params).paginate(:page => params[:page], :per_page => 5)
           
           render layout: nil
         end
