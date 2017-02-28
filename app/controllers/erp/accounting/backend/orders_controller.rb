3c42041315fbb4a7bdbc7c9b8f3b1528fa87ae0e
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-02-27 18:37:08 -0800

    update list action for accounting order controller

--- app/controllers/erp/accounting/backend/orders_controller.rb
+++ app/controllers/erp/accounting/backend/orders_controller.rb
@@ -7,7 +7,7 @@ module Erp
     
         # POST /orders/list
         def list
-          @orders = Erp::Orders::Order.search(params).paginate(:page => params[:page], :per_page => 5)
+          @orders = Erp::Orders::Order.search(params).status_active_for_orders.paginate(:page => params[:page], :per_page => 5)
           
           render layout: nil
         end
