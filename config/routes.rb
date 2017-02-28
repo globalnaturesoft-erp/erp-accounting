3c4d9a45f506a6dac18f790ec8b043f9f423a080
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-02-27 19:32:52 -0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Erp::Accounting::Engine.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
 		namespace :backend, module: "backend", path: "backend/accounting" do
-			resources :orders do
-				collection do
-					post 'list'
-				end
-			end
+			get 'accounting_orders' => 'accountings#accounting_orders', as: :accounting_orders
+			post 'accounting_orders_listing' => 'accountings#accounting_orders_listing', as: :accounting_orders_listing
 		end
 	end
 end
